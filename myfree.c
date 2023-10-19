d50af6de806f0ff5dfa7fd144980dbd85176fba8
hotcuppajojo <joannappetersky@gmail.com>
2023-10-19 13:02:30 -0700

    File setup

---
+++ myfree.c
@@ -0,0 +1,7 @@
+/**
+ * Homework 4 - CS 446/646
+ * File: myfree.c
+ * Author: JoJo Petersky
+ * Last Revision: 2023-10-19
+ */
+
---
+++ mymalloc.c
@@ -0,0 +1,7 @@
+/**
+ * Homework 4 - CS 446/646
+ * File: mymalloc.c
+ * Author: JoJo Petersky
+ * Last Revision: 2023-10-19
+ */
+
